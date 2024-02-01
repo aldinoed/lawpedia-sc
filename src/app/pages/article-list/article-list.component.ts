32f47005f1ae5350325e3306d41f1b5335bbc45f
Bayu Hadi Leksana <bayuhadi863@gmail.com>
2024-02-01 20:19:27 +0700

    Membuat List Artikel dengan Pagination

--- src/app/pages/article-list/article-list.component.ts
+++ src/app/pages/article-list/article-list.component.ts
@@ -6,26 +6,36 @@ import { CommonModule } from '@angular/common';
 import { ArticleService } from '../../services/article.service';
 import { Article } from '../../services/article.service';
 import { RouterModule } from '@angular/router';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-article-list',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgxPaginationModule],
   templateUrl: './article-list.component.html',
-  styleUrl: './article-list.component.css'
+  styleUrl: './article-list.component.css',
 })
-
 export class ArticleListComponent implements OnInit {
   articles: Observable<Article[]> = new Observable<Article[]>();
   popularArticles: Observable<Article[]> = new Observable<Article[]>();
 
-  constructor(
-    private articleService: ArticleService,
-  ) {}
+  //Pagination Settings
+  p: number = 1;
+  collection: Array<any> = []; // contain articles 
+
+  constructor(private articleService: ArticleService) {}
 
   ngOnInit() {
     this.articles = this.articleService.getArticles();
-    this.popularArticles = this.articleService.getPopularArticles(); 
+    this.popularArticles = this.articleService.getPopularArticles();
+
+    // convert observable to object and assign it to collection
+    this.articleService.getArticles().subscribe((articles) => {
+      this.collection = articles.map((article) => ({
+        title: article.title,
+        content: article.content,
+      }));
+    });
+    // console.log(this.collection);
   }
-  
 }
