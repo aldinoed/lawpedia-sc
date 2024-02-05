ea79bfa9009c0682afa0c7d337d90c92aea57276
Bayu Hadi Leksana <bayuhadi863@gmail.com>
2024-02-05 10:58:51 +0700

    Article Detail Done

--- src/app/pages/article-detail/article-detail.component.ts
+++ src/app/pages/article-detail/article-detail.component.ts
@@ -17,6 +17,46 @@ export class ArticleDetailComponent implements OnInit {
   publishedDate: Date = new Date();
   articleCategory = '';
 
+  // RATING COMPONENT
+  getInitialName(name: string): string {
+    const words = name.split(' ');
+    let initials = '';
+    for (let i = 0; i < Math.min(words.length, 2); i++) {
+      initials += words[i].charAt(0);
+    }
+
+    return initials;
+  }
+
+  authenticatedUser: string = 'John Doe';  // Authenticated User Data
+  userNameLength: number = this.authenticatedUser.length;
+  profileBackgroundData: Array<string> = [
+    'bg-gray-800',
+    'bg-gray-500',
+    'bg-red-500',
+    'bg-green-500',
+    'bg-blue-500',
+    'bg-yellow-500',
+    'bg-amber-500',
+    'bg-purple-500',
+    'bg-pink-500',
+    'bg-orange-500',
+    'bg-rose-500',
+    'bg-emerald-500',
+  ];
+
+  getRandomBackground(): string {
+    const length = this.profileBackgroundData.length;
+    const randomIndex = Math.floor(Math.random() * length);
+    return this.profileBackgroundData[randomIndex];
+  }
+
+  ratingValue = 0; // Rating Data
+
+  onRatingClick(value: number) {
+    this.ratingValue = value;
+  }
+
   constructor(
     private route: ActivatedRoute,
     private articleService: ArticleService
