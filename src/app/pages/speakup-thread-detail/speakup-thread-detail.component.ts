fd472f29f76f39a5f7b112844143cafa6a7bb140
Bayu Hadi Leksana <bayuhadi863@gmail.com>
2024-02-11 17:29:49 +0700

    create route for speakup & create speakup component

---
+++ src/app/pages/speakup-thread-detail/speakup-thread-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-speakup-thread-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './speakup-thread-detail.component.html',
+  styleUrl: './speakup-thread-detail.component.css'
+})
+export class SpeakupThreadDetailComponent {
+
+}
---
+++ src/app/pages/speakup-threads-list/speakup-threads-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-speakup-threads-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './speakup-threads-list.component.html',
+  styleUrl: './speakup-threads-list.component.css'
+})
+export class SpeakupThreadsListComponent {
+
+}
