31f02786c24089c6c0abafc5b3c7e4c93aea5f2b
Bayu Hadi Leksana <bayuhadi863@gmail.com>
2024-02-19 20:24:33 +0700

    Slicing dashboard ui

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { initFlowbite } from 'flowbite';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
+  ngOnInit(): void {
+    initFlowbite(); //flowbite initiation
+  }
 
+  
+
+  // ANALYTIC CARD DATA
+  analyticCards: Array<any> = [
+    {
+      name: 'Jumlah artikel terunggah',
+      amount: 755,
+    },
+    {
+      name: 'Jumlah hoaks threads terunggah',
+      amount: 755,
+    },
+    {
+      name: 'Jumlah Speakup threads terunggah',
+      amount: 755,
+    },
+    {
+      name: 'Jumlah Sumber Literasi terunggah',
+      amount: 755,
+    },
+  ];
 }
